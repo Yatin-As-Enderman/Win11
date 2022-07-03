c97827f1a86eeddcf4d90fdb9188c6c473047194
Yatin-As-Enderman <108578826+Yatin-As-Enderman@users.noreply.github.com>
2022-07-03 09:13:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function rufusStatus() {
     const rufus = document.getElementById('rufus_crash_shellwinclassic_runmode-invoker');
     rufus.classList.toggle('active');
 }
+
+function menuStatus() {
+    const menu = document.getElementById('_uwpshell_StartMenuExperience');
+    menu.classList.toggle('active');
+}
