17fdab61694deb5582a325d3a30fc1a5bb9cbf4a
Yatin-As-Enderman <108578826+Yatin-As-Enderman@users.noreply.github.com>
2022-07-08 22:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function menuStatus() {
     const menu = document.getElementById('_uwpshell_StartMenuExperience');
     menu.classList.toggle('active');
 }
+
+function rufusProgressbarStart() {
+    var i = 0;
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById("rufus-status-pBarMain");
+      var width = 1;
+      var id = setInterval(frame, 0.1);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + "%";
+        }
+      }
+    }
+  }
